f978a22e181820a8fb5f1b19bd2083bb182e2e43
Jorge Acereda <jacereda@gmail.com>
2025-05-28 08:06:46 +0200

    Both spaces -> enter

--- keyboards/crkbd/keymaps/jacereda/keymap.c
+++ keyboards/crkbd/keymaps/jacereda/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                                 KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
      KC_1, KC_2, KC_3, KC_4, KC_5,                                                      KC_6, KC_7, KC_8, KC_9, KC_0,
      LGUI_T(KC_F11), LALT_T(KC_F12), LCTL_T(KC_LBRC), LSFT_T(KC_RBRC), RALT_T(KC_SLSH), RALT_T(KC_BSLS), RSFT_T(KC_MINS), RCTL_T(KC_EQL), LALT_T(KC_GRV), RGUI_T(KC_NO),
-     KC_NO, KC_SPC, KC_TAB,                                                             KC_BSPC, KC_SPC, KC_NO),
+     KC_NO, KC_ENT, KC_TAB,                                                             KC_BSPC, KC_ENT, KC_NO),
     [2] = LAYOUT_split_3x5_3(
      KC_NO, KC_BRIU, KC_MNXT, KC_VOLU, KC_MPLY,                                         KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_END,
      KC_NO, KC_BRID, KC_MPRV, KC_VOLD, KC_MSTP,                                         KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
